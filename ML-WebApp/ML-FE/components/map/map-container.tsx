0edbd4ee92ae41c34f3d074b72baf00b5fad2eec
Demian-Moschin <demianmoschin@gmail.com>
2024-07-02 10:20:01 +0200

    feat(CP-01): remove unnecessary api key

--- ML-WebApp/ML-FE/components/map/map-container.tsx
+++ ML-WebApp/ML-FE/components/map/map-container.tsx
@@ -11,7 +11,7 @@ export default function MapContainer({
     <APIProvider
       apiKey={
         process.env.GOOGLE_MAPS_API_KEY ||
-        "AIzaSyBHkxfS_nlqW0_KMpd8H-t47y6GRc_MTEk"
+        ""
       }
     >
       {children}
